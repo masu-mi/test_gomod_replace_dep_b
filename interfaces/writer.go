fb6432136117c7a25abea8eecefd2a6b0f3d0eda
Kanai Masumi <m-kanai@bbsakura.net>
2021-08-02 16:04:34 +0900

    fix mod name in interfaces

--- interfaces/writer.go
+++ interfaces/writer.go
@@ -3,5 +3,5 @@ package interfaces
 import "github.com/masu-mi/test_gomod_replace_dep_b"
 
 type Writer interface {
-	Write(payload string) (num test_gomod_replace_dep_b.OriginalInt)
+	Write(payload string) (num test_gomod_replace_dep_a.OriginalInt)
 }
